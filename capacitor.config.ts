0405738d3706b51158499b8f1021ed1a4d7ddd3f
Amdress cavacal <0515vale0515@gmail.com>
2024-08-23 20:02:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'labs-startup',
+  webDir: 'www'
+};
+
+export default config;
