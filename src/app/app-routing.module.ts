211446ff48070ce23ca7d051454d9566f22bc0c9
Amdress cavacal <0515vale0515@gmail.com>
2024-08-27 21:17:50 -0300

    creando la estrutura de los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'company',
-    loadChildren : () => import('./company-module/company.module').then( m => m.CompanyModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path : 'company',
+    loadChildren: () => import('./modules/company-module/company-module.module').then( m => m.CompanyModuleModule)
+  },
+ 
+
+
 ];
 
 @NgModule({
--- src/app/company-module/company-routing.constant.ts
+++
@@ -1,5 +0,0 @@
-export const COMPANY_ROUTES = {
-    CREATE : 'company-create',
-    EDIT : 'company-edit',
-    PROFILE : 'company/:id'
-};
